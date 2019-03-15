9f0efcbec25cb2cf0011597f09926ffc80600a97
Veronur <luccadelchiaro@gmail.com>
2019-03-14 21:10:12 -0300

    .

---
+++ APS1/src/main.c
@@ -0,0 +1,473 @@
+/**
+ * 5 semestre - Eng. da Computação - Insper
+ * Rafael Corsi - rafael.corsi@insper.edu.br
+ *
+ ** Entrega realizada em parceria com:
+ **  - Lucca Delchiaro Costabile
+ **
+ **  - https://youtu.be/xhRv6FLksJY
+ */
+
+/************************************************************************/
+/* includes                                                             */
+/************************************************************************/
+
+#include "asf.h"
+
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+
+//NOTAS
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+
+//PINOS
+ #define LED_PIO           PIOC                  // periferico que controla o LED
+ #define LED_PIO_ID        12                    // ID do periférico PIOC (controla LED)
+ #define LED_PIO_IDX       8u                    // ID do LED no PIO
+ #define LED_PIO_IDX_MASK  (1u << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+ 
+ #define PAUSE_PIO				PIOA
+ #define PAUSE_PIO_ID			ID_PIOA 
+ #define PAUSE_PIO_IDX			3
+ #define PAUSE_PIO_IDX_MASK		(1u << PAUSE_PIO_IDX)
+ 
+ #define SKIP_PIO				PIOA
+ #define SKIP_PIO_ID			ID_PIOA
+ #define SKIP_PIO_IDX			4
+ #define SKIP_PIO_IDX_MASK		(1u << SKIP_PIO_IDX) 
+ 
+ #define BUZZ_PIO				PIOA
+ #define BUZZ_PIO_ID			ID_PIOA
+ #define BUZZ_PIO_IDX			19
+ #define BUZZ_PIO_IDX_MASK		(1u << BUZZ_PIO_IDX)
+ 
+ 
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+
+int notesPirate[] = {       //Note of the song, 0 is a rest/pulse
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+   
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+
+   NOTE_A4, NOTE_A4, 
+   //Repeat of first part
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+   
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+   //End of Repeat
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+};
+	
+int durationPirate[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 125, 250, 125,
+
+  125, 125, 250, 125, 125, 
+  250, 125, 250, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 375, 375,
+
+  250, 125,
+  //Rpeat of First Part
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 125, 250, 125,
+
+  125, 125, 250, 125, 125, 
+  250, 125, 250, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 375, 375,
+  //End of Repeat
+  
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500,
+
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500
+};
+
+int notesMario[] = {
+	NOTE_E7, NOTE_E7, 0, NOTE_E7,
+	0, NOTE_C7, NOTE_E7, 0,
+	NOTE_G7, 0, 0,  0,
+	NOTE_G6, 0, 0, 0,
+	
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
+	
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0,
+	
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
+	
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0
+};
+
+int durationMario[] = {
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+};
+
+int notesUnderworld[] = {
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+	NOTE_CS4, NOTE_DS4,
+	NOTE_DS4, NOTE_GS3,
+	NOTE_G3, NOTE_CS4,
+	NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+	NOTE_GS4, NOTE_DS4, NOTE_B3,
+	NOTE_AS3, NOTE_A3, NOTE_GS3,
+	0, 0, 0
+};
+
+int durationUnderworld[] = {
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	3,
+	12, 12, 12, 12,
+	12, 12, 6,
+	6, 18, 18, 18,
+	6, 6,
+	6, 6,
+	6, 6,
+	18, 18, 18, 18, 18, 18,
+	10, 10, 10,
+	10, 10, 10,
+	3, 3, 3
+};
+/************************************************************************/
+/* prototypes                                                           */
+/************************************************************************/
+
+void init(void);
+
+/************************************************************************/
+/* interrupcoes                                                         */
+/************************************************************************/
+
+/************************************************************************/
+/* funcoes                                                              */
+/************************************************************************/
+
+// Função de inicialização do uC
+void init(void){
+	// Initialize the board clock
+	sysclk_init();
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(PAUSE_PIO_ID);
+
+	
+	// configura pino ligado ao botão como entrada com um pull-up.
+	pio_set_input(PAUSE_PIO,PAUSE_PIO_IDX_MASK,PIO_DEFAULT);
+	
+	pio_pull_up(PAUSE_PIO,PAUSE_PIO_IDX_MASK,1);
+
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(BUZZ_PIO_ID);
+	pmc_enable_periph_clk(LED_PIO_ID);
+	
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(BUZZ_PIO, BUZZ_PIO_IDX_MASK, 0, 0, 0);
+}
+
+/************************************************************************/
+/* Main                                                                 */
+/************************************************************************/
+
+// Funcao principal chamada na inicalizacao do uC.
+int main(void)
+{
+	// inicializa sistema e IOs
+	init();
+
+	// super loop
+	// aplicacoes embarcadas não devem sair do while(1).
+	int wait;
+	int Notefreq;
+	int track=1;
+	int nNotas;
+	while (1)
+	{
+		if (!(pio_get(SKIP_PIO,PIO_INPUT,SKIP_PIO_IDX_MASK))){
+			track+=1;
+			if (track>3){
+				track=1;
+			}
+			delay_s(1.5);
+		}
+		
+		if(!(pio_get(PAUSE_PIO,PIO_INPUT,PAUSE_PIO_IDX_MASK))){
+			delay_s(1);
+			if (track==1){
+				nNotas=203;
+				
+			}
+			if (track==2){
+				nNotas = sizeof(notesUnderworld) / sizeof(int);
+			}
+			if (track==3){
+				nNotas= sizeof(notesMario) / sizeof(int);
+			}
+			
+			for (int i=0; i<nNotas; i++){
+			if (track==1){
+				wait = durationPirate[i];
+				Notefreq = notesPirate[i];
+
+				
+			}
+			if (track==2){
+				wait= durationUnderworld[i]*6;
+				Notefreq = notesUnderworld[i];
+			}
+			
+			
+			if (track==3){
+				wait = durationMario[i]*25;
+				Notefreq = notesMario[i];
+				
+			}
+			if(!(pio_get(PAUSE_PIO,PIO_INPUT,PAUSE_PIO_IDX_MASK))){
+				delay_s(1);
+				break;
+			}
+				pio_set(PIOC, LED_PIO_IDX_MASK);
+				for (int j=0; j<wait; j++){
+			
+					pio_set(PIOA, BUZZ_PIO_IDX_MASK);
+					    
+					delay_us((((1000000)/Notefreq))/2);                    
+					pio_clear(PIOA, BUZZ_PIO_IDX_MASK); 
+					 
+					delay_us((((1000000)/Notefreq))/2);  
+					
+					
+				}	
+				
+				
+				pio_clear(PIOC, LED_PIO_IDX_MASK);
+				delay_us(wait);	
+				
+				   		
+			}
+		}
+	}
+	return 0;
+}
